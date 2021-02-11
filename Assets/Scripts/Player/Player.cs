0ee256455055292c4ea7efffa1eb91398532eb88
Jason Tidwell <68177963+tidwellj@users.noreply.github.com>
2021-02-11 14:18:06 -0600

    menus

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -2,25 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
+    Material SphereMaterial;
+    public Color newColor;
+
+
     public event Action<Player> onPlayerDeath;
     public int health = 3;
     // Start is called before the first frame update
-
+    // Use this for initialization
+    void Start()
+    {
+        SphereMaterial = Resources.Load<Material>("ya");
+        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+        // Get the current material applied on the GameObject
+        Material oldMaterial = meshRenderer.material;
+        Debug.Log("Applied Material: " + oldMaterial.name);
+        // Set the new material on the GameObject
+        // get color
+        meshRenderer.material = SphereMaterial;
+    }
     void collidedWithEnemy(Enemy enemy)
     {
         enemy.Attack(this);
         if (health <= 0)
         {
-            if (onPlayerDeath != null)
-            {
-                onPlayerDeath(this);
-            }
-        }
-    }
+            
+               
+                if (onPlayerDeath != null)
+                {
 
+                onPlayerDeath(this);
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+
+            }
+
+
+        }
+        
+    }
+    
 
     void OnCollisionEnter(Collision col)
     {
@@ -29,5 +53,24 @@ public class Player : MonoBehaviour
         {
             collidedWithEnemy(enemy);
         }
+    }
+    private void Update()
+    {
+        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+        if (DropD.playerColor == "Red")
+        {
+            meshRenderer.material.color = Color.red;
+        }
+        else if (DropD.playerColor == "Blue")
+        {
+            meshRenderer.material.color = Color.blue;
+        }
+
+        else if (DropD.playerColor == "Green")
+        {
+            meshRenderer.material.color = Color.green;
+        }
+
+        
     }
 }
