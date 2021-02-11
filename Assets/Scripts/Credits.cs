2fe13e2ee4bfa673b3061c30c7a528030d5d3632
Jason Tidwell <68177963+tidwellj@users.noreply.github.com>
2021-02-11 17:56:39 -0600

    More tweaking

--- Assets/Scripts/Credits.cs
+++ Assets/Scripts/Credits.cs
@@ -6,6 +6,8 @@ public class Credits : MonoBehaviour
 
     public void Quit()
     {
+        transform.localScale = new Vector3(1, 1, 1);
+
         Debug.Log("Quit");
         Application.Quit();
     }
--- Assets/Scripts/Replay.cs
+++ Assets/Scripts/Replay.cs
@@ -8,6 +8,8 @@ public class Replay : MonoBehaviour
     // Start is called before the first frame update
     public void restart()
     {
+        transform.localScale = new Vector3(1, 1, 1);
+
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex -2);
 
     }
